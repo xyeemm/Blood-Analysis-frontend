1ecef7e04497f20273341c77b03764ae54b4a78f
xyeem <akselaxel50@gmail.com>
2026-03-21 11:44:05 +0500

    fix: add border styling to header for improved layout consistency

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,7 +7,10 @@ const Layout = () => {
 	return (
 		<div>
 			<header>
-				<div className='sticky flex justify-end px-5 py-2.5 gap-2  shadow-md'>
+				<div
+					className='sticky flex justify-end px-5 py-2.5 gap-2 shadow-md  border-b border-gray-200 dark:border-gray-800 
+'
+				>
 					<div className='flex items-center gap-2 px-3 py-1.5 rounded-full bg-gradient-to-r from-violet-600 to-indigo-600 text-white text-sm font-medium shadow-lg'>
 						<Sparkles className='w-4 h-4 animate-pulse' />
 						<span>Enchanted by AI Alchemy </span>
