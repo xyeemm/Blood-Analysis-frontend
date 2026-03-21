e376d6522997dd31ce4f74f9b161d12be69f7542
xyeem <akselaxel50@gmail.com>
2026-03-21 11:33:04 +0500

    feat: implement theme toggle functionality with dropdown menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/components/darkMode/darkMode.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
@@ -8,7 +9,9 @@ import store from './redux-toolkit/store.ts'
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Provider store={store}>
-			<App />
+			<ThemeProvider>
+				<App />
+			</ThemeProvider>
 		</Provider>
 	</StrictMode>,
 )
