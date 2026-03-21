3cbd1339f1e47f034051213d9d4f0e0dcafdcd6e
xyeem <akselaxel50@gmail.com>
2026-03-21 11:37:56 +0500

    fix: adjust layout structure by repositioning ModeToggle component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,13 +7,13 @@ const Layout = () => {
 	return (
 		<div>
 			<header>
-				<div className='sticky flex justify-end px-5 py-2.5 bg-white shadow-md'>
+				<div className='sticky flex justify-end px-5 py-2.5 gap-2  shadow-md'>
 					<div className='flex items-center gap-2 px-3 py-1.5 rounded-full bg-gradient-to-r from-violet-600 to-indigo-600 text-white text-sm font-medium shadow-lg'>
 						<Sparkles className='w-4 h-4 animate-pulse' />
 						<span>Enchanted by AI Alchemy </span>
 					</div>
+					<ModeToggle />
 				</div>
-				<ModeToggle />
 			</header>
 			<div>
 				<Outlet />
